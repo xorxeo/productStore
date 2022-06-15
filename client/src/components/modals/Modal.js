9b867f3b263f43099ce64891e327a06cae677237
xorxeo <oushoush@mail.ru>
2022-06-15 23:32:00 +0300

    create Modal component to add category or product

--- client/src/components/modals/Modal.js
+++ client/src/components/modals/Modal.js
@@ -1,20 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
-export const Modals = () => {
-  const [show, setShow] = useState(false);
+export const Modal = ({ show, close }) => {
+  // const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  // const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
 
   return (
-    <div>
-      Modals
-      <button onClick={() => {
-
-      }}
-      >
-        open modal
-      </button>
+    <div show={show} close={close} className="modal-background flex w-screen h-screen fixed justify-center items-center bg-slate-100">
+      <div className="modal-container flex flex-col w-96 h-96 rounded bg-slate-200 shadow-lg p-6">
+        <div className="title flex text-center mt-3">Add Product Category</div>
+        <div className="body">
+          <input
+            className="flex "
+            placeholder="Введите название категории"
+          />
+          <input
+            className="flex "
+            placeholder="Загрузите изображение"
+          />
+        </div>
+        <div className="footer">
+          <button onClick={() => {close(false)}}>Cancel</button>
+          <button>Add</button>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { AddProductCategory } from "../components/modals/AddProductCategory";
 import { AddProductItem } from "../components/modals/AddProductItem";
+import { Modal } from "../components/modals/Modal";
+import { useState } from "react";
 
 export const Admin = () => {
+  const [addProductCategoryVisible, setAddProductCategoryVisible] = useState(false);
   return (
     <div className="container-admin flex flex-col h-screen min-w-full pt-14 ">
       <div className="flex flex-col w-full p-2 items-center ">AdminPanel</div>
       <div className="flex flex-row p-2 space-x-4 justify-center ">
-        <button className="p-1 border-2 rounded bg-slate-200 ">Add ProductCategory</button>
+        <button className="p-1 border-2 rounded bg-slate-200 " onClick={() => {setAddProductCategoryVisible("true")}}>Add ProductCategory</button>
         <button className="p-1 border-2 rounded bg-slate-200 ">Add ProductItem</button>
       </div>
-     
+     {addProductCategoryVisible && <Modal show={addProductCategoryVisible} close={setAddProductCategoryVisible}/>}
     </div>
   );
 };
