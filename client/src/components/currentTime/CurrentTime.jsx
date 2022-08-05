865774e21d71c0954ca4a249e1c8c19667938cc5
xorxeo <oushoush@mail.ru>
2022-08-05 19:24:22 +0300

    create currentTime component

--- client/src/components/currentTime/CurrentTime.jsx
+++ client/src/components/currentTime/CurrentTime.jsx
@@ -1,9 +1,20 @@
+import { useState, useEffect } from "react";
 
 export const CurrentTime = () => {
+  const [time, setTime] = useState();
+  const [date, setDate] = useState();
 
-    return (
-      <div className="current-time-wrapper">
-        <div>CurrentTime</div>
-      </div>
-    );
-}
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+      setDate(new Date().toLocaleDateString());
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="current-time-wrapper">
+          <div className="time">{time}</div>
+          {/* <div className="date">{date}</div> */}
+    </div>
+  );
+};
--- client/src/components/modals/BasketModal.js
+++ client/src/components/modals/BasketModal.js
@@ -4,21 +4,18 @@ import { useState, useContext } from "react";
 import { createCategory } from "../../http/categoryAPI";
 import { BasketItem } from "../BasketItem";
 import { Context } from "../../index";
+import { CurrentTime } from "../currentTime/CurrentTime";
 
 export const BasketModal = observer(() => {
   const { user } = useContext(Context);
- 
 
   return (
-    <div
-      className="modal-background flex flex-col  lg:p-10  bg-slate-100 rounded-md z-10"
-    >
-      <div className="flex flex-col w-fit ">
-        <p className="text-lg font-medium">Hello, {user.emailFromLogin}!</p>
-        
+    <div className="modal-background flex flex-col  lg:p-10  bg-slate-100 rounded-md z-10">
+      <div className="flex flex-row justify-between w-full ">
+        <div className="user flex text-lg font-medium">Hello, {user.emailFromLogin}!</div>
+        <div className="clock flex"><CurrentTime /></div>
       </div>
       <div>{<BasketItem />}</div>
     </div>
   );
-}
-)
+});
